2238929f5c24e0dfc35cba626ec4a0f2ebfe6e81
Denhetsu <leomilo719@gmail.com>
2022-04-07 21:15:08 +0200

    oncroiselesdoigts

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from combinaisons import Combinaisons
-from Machine import Machine, firstDraw
+from Machine import Machine
 from time import sleep
 from flask import Flask, render_template, url_for, request, session, redirect
-from Machine import *
-
-
-
-deck = ['2-h','3-h','4-h','5-h','6-h','7-h','8-h','9-h','10-h','J-h','Q-h','K-h','A-h','2-d','3-d','4-d','5-d','6-d','7-d','8-d','9-d','10-d','J-d','Q-d','K-d','A-d','2-c','3-c','4-c','5-c','6-c','7-c','8-c','9-c','10-c','J-c','Q-c','K-c','A-c','2-s','3-s','4-s','5-s','6-s','7-s','8-s','9-s','10-s','J-s','Q-s','K-s','A-s']
 
 
 app = Flask(__name__)
@@ -44,11 +39,6 @@ def game():
     session ['error-message'] = "Désolé mais votre mise est supérieure à l'argent sur votre compte"
     return render_template('game.html')
   else:
-    drawed_cards, newDeck  = firstDraw(deck)
-    session["drawed_cards"] = drawed_cards
-    session["deck"] = newDeck
-    
-
     return redirect(url_for('draw'))
 
 
@@ -62,6 +52,9 @@ def draw():
 
 if __name__ == '__main__':
   app.run(debug = True)
+
+
+
 
 
 
